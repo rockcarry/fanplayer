a6ed455f3258e175032a0a509106681625d9b243
chenk <chenk@apical.com.cn>
2018-03-12 18:22:44 +0800

    add soundtouch win32 library.

---
+++ player-win32/soundtouch-win32/include/soundtouch/soundtouch_config.h
@@ -0,0 +1,6 @@
+/* include/soundtouch_config.h.  Generated from soundtouch_config.h.in by configure.  */
+/* Use Float as Sample type */
+#define SOUNDTOUCH_FLOAT_SAMPLES 1
+
+/* Use Integer as Sample type */
+/* #undef SOUNDTOUCH_INTEGER_SAMPLES */
