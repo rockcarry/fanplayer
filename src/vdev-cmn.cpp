ecf0ff3f56545cb7d5ac8cb440caff6d9487bc41
chenk <chenk@apical.com.cn>
2018-06-04 18:06:13 +0800

    优化 vdev-cmn.cpp 中 vdev_avsync_and_complete 的代码

--- src/vdev-cmn.cpp
+++ src/vdev-cmn.cpp
@@ -163,7 +163,7 @@ void vdev_avsync_and_complete(void *ctxt)
             c->completed_vpts = c->vpts;
             c->completed_counter = 0;
             c->status &=~VDEV_COMPLETED;
-        } else if ((c->vpts == -1 || c->vpts == AV_NOPTS_VALUE) && ++c->completed_counter == COMPLETED_COUNTER) {
+        } else if (++c->completed_counter == COMPLETED_COUNTER) {
             c->status |= VDEV_COMPLETED;
             player_send_message(c->surface, MSG_PLAY_COMPLETED, 0);
         }
