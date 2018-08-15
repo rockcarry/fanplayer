20a91361c3342e0f7b43cc809545e893ba6b3111
chenk <chenk@apical.com.cn>
2018-08-15 20:45:15 +0800

    improve code for player_prepare

--- src/ffplayer.cpp
+++ src/ffplayer.cpp
@@ -374,8 +374,11 @@ static int player_prepare(PLAYER *player)
     player->avformat_context->interrupt_callback.opaque   = player;
 
     // open input file
-//  av_dict_set(&opts, "rtsp_transport", "udp", 0);
+    /*
+    av_dict_set(&opts, "rtsp_transport", "udp", 0);
     av_dict_set(&opts, "buffer_size", "1048576", 0);
+    av_dict_set(&opts, "fpsprobesize", "2", 0);
+    */
     if (player->init_params.video_vwidth != 0 && player->init_params.video_vheight != 0) {
         char vsize[64];
         sprintf(vsize, "%dx%d", player->init_params.video_vwidth, player->init_params.video_vheight);
