ecbd1243366b13e919b5f234ef5a88811cab2602
chenk <chenk@apical.com.cn>
2020-11-20 14:28:16 +0800

    修改 ffrdpc.c 中 server lost 的判断超时为 5s

--- ffrdpdemuxer/ffrdpc.c
+++ ffrdpdemuxer/ffrdpc.c
@@ -90,7 +90,7 @@ static void* ffrdpc_thread_proc(void *argv)
         }
 
         ffrdp_update(ffrdpc->ffrdp);
-        if ((ffrdpc->status & TS_CONNECTED) && (int32_t)get_tick_count() - (int32_t)tickgetframe > 3000) {
+        if ((ffrdpc->status & TS_CONNECTED) && (int32_t)get_tick_count() - (int32_t)tickgetframe > 5000) {
             printf("server lost !\n");
             ffrdp_free(ffrdpc->ffrdp); ffrdpc->ffrdp = NULL;
             ffrdpc->head = ffrdpc->tail = ffrdpc->size = 0;
